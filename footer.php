62ee04a8e5711487e70944c63e3e8296e8bee091
Cheryta <98745090+Cheryta@users.noreply.github.com>
2022-04-14 15:37:31 +0200

    ajout d'autres partie

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="footer bg-primary p-2">
+        <p>Copyright@2022 | Designed with by VisaSante</p>
+    </div>
+</footer>
---
+++ menu.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css" media="screen" type="text/css" />
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+</head>
+<body>
+
+    <nav class="navbar  navbar-light bg-primary">
+        <div class="container-fluid m-1 d-flex ">
+            <img src="./pictures/logo1.jpg" alt="" width="200px" height="80px">
+            <div class="text">
+                <a href="index.php">Acceuil</a>
+            </div>
+            <div class="text">
+                <a href="patient.php">Patients</a>
+            </div>
+            <div class="text">
+                <a href="liste.php">Liste des patients</a>
+            </div>
+            <div class="recherche"> 
+                <input id="myInput" onkeyup="myFunction()" type="search"  aria-label="Search"> 
+                <button type="submit" bg-primary >Search</button>
+            </div>
+        </div>
+      </nav>
+
+   <script src="./js/bootstrap.bundle.js"></script>
+   <script src="./js/bootstrap.bundle.min.js"></script>
+   <script src="./js/bootstrap.min.js"></script> 
+</body>
+</html>
